2b2f04db6fb6349a4c588b5a7f2ce133598d795a
Howard Ding <hding2@illinois.edu>
2014-10-29 11:30:45 -0500

    Requiring jars wasn't working with require_relative, so do it this way.

--- lib/amazon_jar_requires.rb
+++ lib/amazon_jar_requires.rb
@@ -1,6 +1,10 @@
 require 'java'
 
-require_relative '../aws-java-sdk-1.8.0/lib/aws-java-sdk-1.8.0.jar'
-Dir[File.join('../aws-java-sdk-1.8.0/third-party/**/*.jar')].each do |jar|
-  require_relative jar
+#Requiring the jars with require_relative wasn't working properly,
+#so we do it this way.
+Dir.chdir(File.dirname(__FILE__)) do
+  require '../aws-java-sdk-1.8.0/lib/aws-java-sdk-1.8.0.jar'
+  Dir[File.join('../aws-java-sdk-1.8.0/third-party/**/*.jar')].each do |jar|
+    require jar
+  end
 end
