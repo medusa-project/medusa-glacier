516d738a4cf23c75b1829eb5a65c5c0630f51527
Howard Ding <hding2@illinois.edu>
2015-08-24 15:19:07 -0500

    Use relative require

--- medusa_glacier.rb
+++ medusa_glacier.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-require 'lib/medusa_glacier_server'
+require_relative 'lib/medusa_glacier_server'
 
 #only run if given run as the first argument. This is useful for letting us load this file
 #in irb to work with things interactively when we need to
