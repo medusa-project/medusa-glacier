74ba5778a004ce19e5c192f67d93c0d4b0c436bd
Howard Ding <hding2@illinois.edu>
2016-04-13 15:44:47 -0500

    Update aws java sdk

--- lib/amazon_jar_requires.rb
+++ lib/amazon_jar_requires.rb
@@ -3,8 +3,8 @@ require 'java'
 #Requiring the jars with require_relative wasn't working properly,
 #so we do it this way.
 Dir.chdir(File.dirname(__FILE__)) do
-  require '../aws-java-sdk-1.8.0/lib/aws-java-sdk-1.8.0.jar'
-  Dir[File.join('../aws-java-sdk-1.8.0/third-party/**/*.jar')].each do |jar|
+  require '../aws-java-sdk-1.10.69/lib/aws-java-sdk-1.10.69.jar'
+  Dir[File.join('../aws-java-sdk-1.10.69/third-party/**/*.jar')].each do |jar|
     require jar
   end
 end
