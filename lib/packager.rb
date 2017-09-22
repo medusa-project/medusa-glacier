8fbeb3e6342ea5e05a91f47f8bd83fd25ad997ea
Howard Ding <hding2@illinois.edu>
2017-09-21 20:09:55 -0500

    explicitly close leveldb

--- lib/packager.rb
+++ lib/packager.rb
@@ -183,7 +183,9 @@ class Packager < Object
   end
 
   def with_manifest_db
-    yield LevelDb.open(MANIFEST_DB_DIR)
+    db = LevelDb.open(MANIFEST_DB_DIR)
+    yield db
+    db.close
   ensure
     FileUtils.rm_rf(MANIFEST_DB_DIR) if Dir.exist?(MANIFEST_DB_DIR)
   end
